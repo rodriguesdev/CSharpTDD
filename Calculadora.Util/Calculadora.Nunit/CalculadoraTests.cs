f9b12e2d6708ef08558f7072d7d8b8ae595269b7
Leandro Rodrigues <leandro.lacr@yahoo.com.br>
2017-04-16 18:00:27 -0300

    Calculadora Nunit

--- Calculadora.Util/Calculadora.Nunit/CalculadoraTests.cs
+++ Calculadora.Util/Calculadora.Nunit/CalculadoraTests.cs
@@ -11,18 +11,32 @@ namespace Calculadora.Nunit
     [TestFixture]
     public class CalculadoraTests
     {
+        private testCalc.Calculadora _calc;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            _calc = new testCalc.Calculadora();
+        }
+
         [Test]
         public void TestSomar()
         {
-            testCalc.Calculadora calc = new testCalc.Calculadora();
-            Assert.AreEqual(4, calc.Somar(2, 2));
+            //testCalc.Calculadora calc = new testCalc.Calculadora();
+            Assert.AreEqual(4, _calc.Somar(2, 2));
         }
 
         [Test]
         public void TestSubtrair()
         {
-            testCalc.Calculadora calc = new testCalc.Calculadora();
-            Assert.AreEqual(0, calc.Subtrair(2, 2));
+            //testCalc.Calculadora calc = new testCalc.Calculadora();
+            Assert.AreEqual(0, _calc.Subtrair(2, 2));
+        }
+
+        [TestFixtureTearDown]
+        public void TerDown()
+        {
+            _calc = null;
         }
 
         //[Test]
