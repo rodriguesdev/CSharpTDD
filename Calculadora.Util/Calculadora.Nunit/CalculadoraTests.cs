f86720f07cd1255879ff962e6c65f0eb2d84c036
Leandro Rodrigues <leandro.lacr@yahoo.com.br>
2017-04-16 17:53:40 -0300

    Nunit

---
+++ Calculadora.Util/Calculadora.Nunit/CalculadoraTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using testCalc = Calculadora.Util;
+
+namespace Calculadora.Nunit
+{
+    [TestFixture]
+    public class CalculadoraTests
+    {
+        [Test]
+        public void TestSomar()
+        {
+            testCalc.Calculadora calc = new testCalc.Calculadora();
+            Assert.AreEqual(4, calc.Somar(2, 2));
+        }
+
+        [Test]
+        public void TestSubtrair()
+        {
+            testCalc.Calculadora calc = new testCalc.Calculadora();
+            Assert.AreEqual(0, calc.Subtrair(2, 2));
+        }
+
+        //[Test]
+        //public void TestMultiplicar()
+        //{
+        //}
+
+        //[Test]
+        //public void TestDividir()
+        //{
+        //}
+    }
+}
